0a14dcb0f7054b92d31b0dc8759a4e4daa14d8e5
iamaashwin99 <iamaashwin99@gmail.com>
2020-10-19 14:20:21 +0530

    datepickker,finalvalue,others

--- main.py
+++ main.py
@@ -16,11 +16,10 @@ from streamlit_ace import st_ace
 import random, string
 import importlib
 st.beta_set_page_config(page_title='Back-plt', page_icon=None, layout='centered', initial_sidebar_state='auto')
-LANGUAGES = [
-     "python",
-]
+
+
 THEMES = [
-    "ambiance", "chaos", "chrome", "clouds", "clouds_midnight", "cobalt", "crimson_editor", "dawn",
+    "chaos","ambiance",  "chrome", "clouds", "clouds_midnight", "cobalt", "crimson_editor", "dawn",
     "dracula", "dreamweaver", "eclipse", "github", "gob", "gruvbox", "idle_fingers", "iplastic",
     "katzenmilch", "kr_theme", "kuroir", "merbivore", "merbivore_soft", "mono_industrial", "monokai",
     "nord_dark", "pastel_on_dark", "solarized_dark", "solarized_light", "sqlserver", "terminal",
@@ -35,7 +34,7 @@ KEYBINDINGS = [
 st.write("""
 # Simple backtrader backtesting App
 * After choosing each parameter press enter.
-* Interval length need to be grater than 1h if the date range is greater than 60 days
+* Interval length need to be grater than 1h if the date range is greater than  LAST 60 days
 * After the chart loads the side bar will load with the backtested data, click on it to download
 """ )
 
@@ -45,9 +44,18 @@ st.write("""
 # interval_length = '5m'
 # start_cash =  '100000.0'
 
+
 tickerSymbol = st.text_input("Enter Symbol (like INFY.NS) ", 'RELIANCE.NS') 
-start_date = st.text_input("Enter Start Date", '2020-09-20') 
-end_date = st.text_input("Enter End Date", '2020-10-18') 
+# start_date = st.text_input("Enter Start Date", '2020-09-20') 
+# end_date = st.text_input("Enter End Date", '2020-10-18') 
+deltaDays = st.text_input("Enter delta days", '30') 
+s_date = st.date_input('Enter Start Date (takes precedence over delta days)', datetime.datetime.now()-datetime.timedelta(days = int(deltaDays)) )
+e_date = st.date_input('Enter End Date', datetime.datetime.now())
+
+start_date = s_date.strftime("%Y-%m-%d")
+end_date = e_date.strftime("%Y-%m-%d")
+
+
 interval_length = st.text_input("Enter Interval length (1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo)", '5m') 
 start_cash = st.text_input("Enter starting Cash", '100000.0') 
 tickerData = yf.Ticker(tickerSymbol)
@@ -70,17 +78,18 @@ with io.open('strategy.py', 'r', encoding='utf8') as f:
 
 content = st_ace(
     value=text,
-    language=st.sidebar.selectbox("Language mode", options=LANGUAGES),
+    language="python",
     theme=st.sidebar.selectbox("Theme", options=THEMES),
     keybinding=st.sidebar.selectbox("Keybinding mode", options=KEYBINDINGS),
     font_size=st.sidebar.slider("Font size", 5, 24, 12),
-    tab_size=st.sidebar.slider("Tab size", 1, 8, 4),
+    tab_size=4,
     show_gutter=st.sidebar.checkbox("Show gutter", value=True),
     show_print_margin=st.sidebar.checkbox("Show print margin", value=True),
-    wrap=st.sidebar.checkbox("Wrap enabled", value=False),
+    wrap=st.sidebar.checkbox("Wrap enabled", value=True),
     readonly=st.sidebar.checkbox("Read-only", value=False, key="ace-editor"),
-    auto_update=st.sidebar.checkbox("Auto update", value=False),
-    key="ace-editor"
+    auto_update=False,
+    key="ace-editor" 
+    
 )
 #exec(content)
 strategy_name = ''.join(random.choices(string.ascii_letters + string.digits, k=8)) 
@@ -102,6 +111,7 @@ def runStrategy(tickerSymbol,tickerDf,start_cash):
     cerebro.adddata(bt.feeds.PandasData(dataname=tickerDf))
     cerebro.addanalyzer(bt.analyzers.SharpeRatio)
     cerebro.run()
+    st.write('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
     #cerebro.plot(style='candlestick', barup='green', bardown='red')
@@ -126,5 +136,6 @@ st.write("""
 """)
 if os.path.exists(strategy_name+'.py'):
   os.remove(strategy_name+'.py')
+  print("File Removed : "+strategy_name+'.py')
 else:
   print("The file does not exist")
